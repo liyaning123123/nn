8149aaec32c4cf44761fc6d1de50efaab93d6aa8
liyaning123123 <15896601390@163.com>
2019-08-07 08:54:32 +0800

    dsd

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+     <button @click="forFn">点击</button>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+
+  },
+  components:{
+
+  },
+  data(){
+    return {
+
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+   forFn(){
+     let data=[{
+       name:"12"
+     },
+     {
+       name:"12"
+     },{
+       name:"12"
+     }]
+
+     for(let i=0;i<data.length;i++){
+      
+         for(let key in data[i]){
+           let c=data[i]
+              console.log(data[i])
+              console.log(key)
+              console.log(c.name)
+     }}
+   }
+  },
+  created(){
+
+  },
+  mounted(){
+
+  }
+}
+</script>
+<style scoped lang="">
+
+</style>
